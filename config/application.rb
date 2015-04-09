1a31efc07f74226bf08bcd0ef39f4e4eb8e59d9a
Hiren.Bhalani-BTC <hiren.bhalani@beaverlogic.com>
2015-04-09 16:40:20 +0530

    first commit

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ShopifyApp
+  class Application < Rails::Application
+
+    # Shopify API connection credentials:
+    config.shopify.api_key = 'dd5001b1ad736777d67e702d00338e20'
+    config.shopify.secret = 'ed892fe29b687959d10afcb739366128'
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
